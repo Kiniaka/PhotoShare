7d5c74bd6dbc6f0cfd9a124d0449451d1a241ba1
Kiniaka <147816937+Kiniaka@users.noreply.github.com>
2024-07-13 10:42:59 +0200

    update repo -  class Opinion added in schema file

--- src/schemas.py
+++ src/schemas.py
@@ -4,16 +4,19 @@ from pydantic import BaseModel, Field, EmailStr
 
 '''------------------------Images-------------------------'''
 
+
 class ImageModel(BaseModel):
     image_name: str
     image_link: str
     user_id: int
     tags: Optional[List[int]]
 
+
 class ImageUpdate(BaseModel):
     image_name: Optional[str]
     image_link: Optional[str]
     tags: Optional[List[int]]
+
 
 class ImageInDB(ImageModel):
     id: int
@@ -21,27 +24,35 @@ class ImageInDB(ImageModel):
     class Config:
         orm_mode = True
 
+
 '''------------------------Notes--------------------------'''
 
+
 class NoteModel(BaseModel):
-    description: str
+    note_description: str
     image_id: int
+
 
 class NoteUpdate(NoteModel):
     updated_at: datetime
 
+
 '''------------------------Tags---------------------------'''
+
 
 class TagModel(BaseModel):
     tag_name: str
     user_id: int
 
+
 '''------------------------Users--------------------------'''
+
 
 class UserModel(BaseModel):
     username: str = Field(min_length=5, max_length=20)
     email: EmailStr
     password: str = Field(min_length=5, max_length=15)
+
 
 class UserDB(UserModel):
     id: int
@@ -53,13 +64,24 @@ class UserDB(UserModel):
     class Config:
         orm_mode = True
 
+
 class UserResponse(BaseModel):
     user: UserDB
     detail: str = 'User succesfully created'
 
+
 '''------------------------Tags---------------------------'''
+
 
 class TokenModel(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = 'bearer'
+
+
+'''------------------------Opinion---------------------------'''
+
+
+class Opinion(BaseModel):
+    vote: int
+    image_id: int
