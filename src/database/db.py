0a0e8ab96eb7660fb487d4917134152b2ec435cb
Kiniaka <147816937+Kiniaka@users.noreply.github.com>
2024-07-17 11:48:42 +0200

    update repo - correction connection with database

--- src/database/db.py
+++ src/database/db.py
@@ -2,14 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+path = 'src/database/.env'
+
+load_dotenv(path)
 
 SQLALCHEMY_DATABASE_URL = (
     f'postgresql+psycopg2://{os.getenv("POSTGRES_USERNAME")}:'
     f'{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("HOST")}:'
     f'{os.getenv("PORT_NR")}/{os.getenv("DATABASE_NAME")}'
 )
+
+# SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{os.getenv("POSTGRES_USERNAME")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("HOST")}:{os.getenv("PORT_NR")}/{os.getenv("DATABASE_NAME")}'
 
 # fake data for sphinx to avoid None data during doc generation
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/postgres"
